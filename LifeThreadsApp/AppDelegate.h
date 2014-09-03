2c64c61f6380d8b9040bdb0b917a564208b78fc5
Terry Bennett <Terry@projectb.biz>
2014-09-03 15:01:24 -0500

    Initial Commit

---
+++ LifeThreadsApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LifeThreadsApp
+//
+//  Created by TJ Bennett on 9/3/14.
+//  Copyright (c) 2014 projectb.biz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LifeThreadsApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  LifeThreadsApp
+//
+//  Created by TJ Bennett on 9/3/14.
+//  Copyright (c) 2014 projectb.biz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
